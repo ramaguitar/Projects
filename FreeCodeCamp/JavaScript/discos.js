377a2ee5b4f655e8074e0f285e7f1a5a416a5676
ramaguitar <ramiguitar2000@hotmail.com.ar>
2023-06-05 20:12:08 -0300

    Practica de POO en JS

---
+++ FreeCodeCamp/JavaScript/discos.js
@@ -0,0 +1,38 @@
+// Configuración
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  function updateRecords(records, id, prop, value) {
+    if(value == ""){
+        delete records[id][prop];
+    }
+    else if(prop != "tracks"){
+        records[id][prop]=value;
+    }else{
+        if (typeof records[id][prop] === 'undefined') { //if (records[id].hasOwnProperty("tracks") === false)
+            records[id][prop] = [];
+        }
+        records[id][prop].push(value);
+    }    
+    return records;
+  }
+  
+   const r = updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
+   console.log(r);
